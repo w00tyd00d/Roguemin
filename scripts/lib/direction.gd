6239f2734692a6a003ea6193e83faad8da638e37
w00t <w00tyd00d@gmail.com>
2025-03-01 10:37:31 -0500

    Changed Direction module to work with integers instead of floats

--- scripts/lib/direction.gd
+++ scripts/lib/direction.gd
@@ -3,25 +3,25 @@ class_name Direction extends Object
 ## Abstract class containing relative vector data for each cardinal and
 ## diagonal direction.
 
-const ALL_VECTORS : Array[Vector2] = [
-    Vector2(-1,-1),
-    Vector2.UP,
-    Vector2(1,-1),
-    Vector2.RIGHT,
-    Vector2(1,1),
-    Vector2.DOWN,
-    Vector2(-1,1),
-    Vector2.LEFT,
+const ALL_VECTORS : Array[Vector2i] = [
+    Vector2i(-1,-1),
+    Vector2i.UP,
+    Vector2i(1,-1),
+    Vector2i.RIGHT,
+    Vector2i(1,1),
+    Vector2i.DOWN,
+    Vector2i(-1,1),
+    Vector2i.LEFT,
 ]
 
 
-static func get_adjacent(vec: Vector2, from := Vector2()) -> Array[Vector2]:
+static func get_adjacent(vec: Vector2i, from := Vector2i()) -> Array[Vector2i]:
     var i := ALL_VECTORS.find(vec)
     assert(i != -1, "Invalid direction vector given.")
     return [from + ALL_VECTORS[(i-1) % 8], from + ALL_VECTORS[(i+1) % 8]]
 
 
-static func get_orthagonal(vec: Vector2, from := Vector2()) -> Array[Vector2]:
+static func get_orthagonal(vec: Vector2i, from := Vector2i()) -> Array[Vector2i]:
     var i := ALL_VECTORS.find(vec)
     assert(i != -1, "Invalid direction vector given.")
     return [from + ALL_VECTORS[(i-2) % 8], from + ALL_VECTORS[(i+2) % 8]]
