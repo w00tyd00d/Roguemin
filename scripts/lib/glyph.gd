7d6c38f43356b160b20c82b1b9321bc9b2a8883f
w00t <w00tyd00d@gmail.com>
2025-03-01 17:52:05 -0500

    Added get_from static function to Glyph

--- scripts/lib/glyph.gd
+++ scripts/lib/glyph.gd
@@ -1,6 +1,8 @@
-class_name Glyph extends Object
+class_name Glyph extends RefCounted
 
 ## Static glyph database for abstract tileset references.
+
+# static var GRASS := Glyph.new()
 
 var source: int
 var atlas_coordinates: Vector2
@@ -13,7 +15,14 @@ func _init(src: int, atlas_coords: Vector2, alt_tile_id := 0) -> void:
     alternative_tile_id = alt_tile_id
 
 
-func matches(src: int, atlas_coords: Vector2, alt_tile_id := 0) -> bool:
-    return (source == src and
-            atlas_coordinates == atlas_coords and
-            alternative_tile_id == alt_tile_id)
+static func get_from(map: TileMapLayer, pos: Vector2i) -> Glyph:
+    var src := map.get_cell_source_id(pos)
+    var atlas_coords := map.get_cell_atlas_coords(pos)
+    var alt_tile := map.get_cell_alternative_tile(pos)
+    return Glyph.new(src, atlas_coords, alt_tile)
+
+
+func matches(glyph: Glyph) -> bool:
+    return (source == glyph.src and
+            atlas_coordinates == glyph.atlas_coords and
+            alternative_tile_id == glyph.alt_tile_id)
