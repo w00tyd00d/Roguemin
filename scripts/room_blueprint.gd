e68abecc2b00b60c70b83d3b600b32865e31bf1b
w00t <w00tyd00d@gmail.com>
2025-03-01 17:54:15 -0500

    Created basic room blueprint object/tool to create prefab rooms

---
+++ scripts/room_blueprint.gd
@@ -0,0 +1,47 @@
+@tool
+
+class_name RoomBlueprint extends TileMapLayer
+
+## A [TileMapLayer] node that automatically fetches and aggregates
+## tilemap data to be used later, such as instantiating setpieces.
+
+## The size, in world nodes, the room consists of
+@export var room_size : Vector2i :
+    set(vec):
+        room_size = vec
+        queue_redraw()
+        
+
+## The dictionary of tile positions and their respective glyph information.
+var tile_data := {}
+
+
+func _init() -> void:
+    _scan()
+    
+
+func _draw() -> void:
+    if not Engine.is_editor_hint(): return
+    
+    const BORDER_THICKNESS := 1
+    const BORDER_HALF_VEC := Vector2(BORDER_THICKNESS / 2.0, BORDER_THICKNESS / 2.0)
+    
+    const NODE_SIZE_OUTER := Vector2i(23, 23) * Globals.TILE_SIZE
+    const NODE_SIZE_INNER := Vector2i(15, 15) * Globals.TILE_SIZE
+
+    var rect1_w := room_size.x * NODE_SIZE_OUTER.x + BORDER_THICKNESS
+    var rect1_h := room_size.y * NODE_SIZE_OUTER.y + BORDER_THICKNESS
+
+    var rect2_w := room_size.x * NODE_SIZE_INNER.x + ((room_size.x - 1) * 64) + BORDER_THICKNESS
+    var rect2_h := room_size.y * NODE_SIZE_INNER.y + ((room_size.y - 1) * 64) + BORDER_THICKNESS
+
+    var rect1 := Rect2(-BORDER_HALF_VEC, Vector2(rect1_w, rect1_h))
+    var rect2 := Rect2(Vector2(Globals.TILE_SIZE) * 4 - BORDER_HALF_VEC, Vector2(rect2_w, rect2_h))
+    draw_rect(rect1, Color.RED, false, BORDER_THICKNESS)
+    draw_rect(rect2, Color.GREEN, false, BORDER_THICKNESS)
+
+
+func _scan() -> void:
+    for vec in get_used_cells():
+        tile_data[vec] = Glyph.get_from(self, vec)
+
