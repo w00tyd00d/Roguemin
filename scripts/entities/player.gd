2a43b0843eccbb1d81b125b367038f3e32f355af
w00t <w00tyd00d@gmail.com>
2025-03-02 15:56:09 -0500

    Added Camera and HUD

--- scripts/entities/player.gd
+++ scripts/entities/player.gd
@@ -2,9 +2,18 @@ class_name Player extends Entity
 
 ## The player object.
 
+@onready var camera := $Camera2D as Camera2D
+
+
 static func create() -> Player:
     return preload("res://prefabs/entities/player.tscn").instantiate()
 
 
 func finish_turn(_time_units: int) -> void:
     pass
+
+
+func move_to(dest: Tile) -> void:
+    super(dest)
+    camera.align()
+    
--- scripts/main.gd
+++ scripts/main.gd
@@ -2,18 +2,20 @@ extends Node
 
 var world_factory := WorldFactory.new()
 
+@onready var game_viewport := $GameScreen/%SubViewport as SubViewport
+
 func _ready() -> void:
     # Set the default background color to black at runtime
     RenderingServer.set_default_clear_color(Color.BLACK)
 
     # Immediately create new world for now
     var world := world_factory.create_new_world()
-    add_child(world)
+    game_viewport.add_child(world)
     world_factory.setup_world(world)
 
     # Immediate create the player character for now
     var player := Player.create()
-    add_child(player)
+    game_viewport.add_child(player)
 
     GameState.world = world
     GameState.player = player
