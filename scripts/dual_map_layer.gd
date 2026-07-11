9c0f5b41b369c5457478e9ee76c6814386e015b7
w00t <w00tyd00d@gmail.com>
2026-07-11 12:37:54 -0400

    Re-typed organization nodes in World scene

--- scripts/dual_map_layer.gd
+++ scripts/dual_map_layer.gd
@@ -12,46 +12,50 @@ var grid_position : Vector2i : set = _set_grid_position
 
 
 static func create() -> DualMapLayer:
-    return preload("res://prefabs/dual_map_layer.tscn").instantiate()
+	return preload("res://prefabs/dual_map_layer.tscn").instantiate()
+
+
+func _init() -> void:
+	process_mode = Node.PROCESS_MODE_DISABLED
 
 
 func _ready() -> void:
-    process_mode = Node.PROCESS_MODE_DISABLED
+	pass
 
 
 func clear_glyphs() -> void:
-    clear()
-    background_layer.clear()
+	clear()
+	background_layer.clear()
 
 
 func set_glyph(pos: Vector2i, glyph: Glyph) -> void:
-    _set_tile(self, pos, glyph)
+	_set_tile(self, pos, glyph)
 
 
 func get_glyph(pos: Vector2i) -> Glyph:
-    return _get_tile(self, pos)
+	return _get_tile(self, pos)
 
 
 func set_background(pos: Vector2i, glyph: Glyph):
-    _set_tile(background_layer, pos, glyph)
+	_set_tile(background_layer, pos, glyph)
 
 
 func get_background(pos: Vector2i):
-    return _get_tile(background_layer, pos)
+	return _get_tile(background_layer, pos)
 
 
 func _set_grid_position(pos: Vector2i) -> void:
-    grid_position = pos
-    position = pos * Globals.TILE_SIZE
+	grid_position = pos
+	position = pos * Globals.TILE_SIZE
 
 
 func _set_tile(layer: TileMapLayer, pos: Vector2i, glyph: Glyph) -> void:
-    layer.set_cell(
-        pos,
-        glyph.source,
-        glyph.atlas_pos,
-        glyph.alt_tile_id)
+	layer.set_cell(
+		pos,
+		glyph.source,
+		glyph.atlas_pos,
+		glyph.alt_tile_id)
 
 
 func _get_tile(layer: TileMapLayer, pos: Vector2i) -> Glyph:
-    return Glyph.get_from(layer, pos)
+	return Glyph.get_from(layer, pos)
--- scripts/entities/entity.gd
+++ scripts/entities/entity.gd
@@ -5,18 +5,18 @@ class_name Entity extends DualMapLayer
 const STEP_COST := Globals.DEFAULT_TURN_COST
 
 var RNG : RandomNumberGenerator :
-    get: return GameState.RNG
+	get: return GameState.RNG
 
 var world : World :
-    get: return GameState.world
+	get: return GameState.world
 
 var player : Player :
-    get: return GameState.player
+	get: return GameState.player
 
 var current_tile : Tile :
-    get:
-        if not world: return null
-        return world.get_tile(grid_position)
+	get:
+		if not world: return null
+		return world.get_tile(grid_position)
 
 ## The cached last position of the entity.
 var last_position : Vector2i
@@ -26,7 +26,7 @@ var last_direction : Direction
 
 ## The last vector the entity had traveled
 var last_velocity : Vector2i :
-    get: return last_direction.vector
+	get: return last_direction.vector
 
 ## The brain of the entity
 var brain : Brain
@@ -36,76 +36,76 @@ var entity_name := "Unknown Entity"
 
 # ## The value of time the entity has been synced up to.
 var time : int :
-    set(n): if brain: brain.time = n
-    get: return brain.time if brain else 0
+	set(n): if brain: brain.time = n
+	get: return brain.time if brain else 0
 
 ## Flag for signaling if the entity can act on this turn.
 var can_act : bool :
-    get: return brain.can_act if brain else true
+	get: return brain.can_act if brain else true
 
 ## Dictionary of immunities the entity has.
 var _immunities := {}
 
 
 func _ready() -> void:
-    super()
+	super()
 
 
 func move_to(dest: Tile) -> void:
-    world.move_entity(self, dest)
+	world.move_entity(self, dest)
 
 
 func move_towards(target: Tile) -> bool:
-    var delta := target.grid_position - grid_position
-    var ax := absi(delta.x)
-    var ay := absi(delta.y)
+	var delta := target.grid_position - grid_position
+	var ax := absi(delta.x)
+	var ay := absi(delta.y)
 
-    var vec : Vector2i
+	var vec : Vector2i
 
-    if ax >= ay * 2: vec = Vector2i(delta.sign().x, 0)
-    elif ay >= ax * 2: vec = Vector2i(0, delta.sign().y)
-    else: vec = delta.sign()
+	if ax >= ay * 2: vec = Vector2i(delta.sign().x, 0)
+	elif ay >= ax * 2: vec = Vector2i(0, delta.sign().y)
+	else: vec = delta.sign()
 
-    var dir := Direction.by_pattern(vec)
+	var dir := Direction.by_pattern(vec)
 
-    if world.query_tile_at(grid_position + dir.vector) == Type.Tile.GRASS:
-        var dest = world.get_tile(grid_position + dir.vector)
-        move_to(dest)
-        return true
+	if world.query_tile_at(grid_position + dir.vector) == Type.Tile.GRASS:
+		var dest = world.get_tile(grid_position + dir.vector)
+		move_to(dest)
+		return true
 
-    for _dir in dir.adjacent:
-        if grid_position + _dir.vector == last_position: continue
-        if world.query_tile_at(grid_position + _dir.vector) == Type.Tile.GRASS:
-            var dest := world.get_tile(grid_position + _dir.vector)
-            move_to(dest)
-            return true
+	for _dir in dir.adjacent:
+		if grid_position + _dir.vector == last_position: continue
+		if world.query_tile_at(grid_position + _dir.vector) == Type.Tile.GRASS:
+			var dest := world.get_tile(grid_position + _dir.vector)
+			move_to(dest)
+			return true
 
-    return false
+	return false
 
 
 func update() -> bool:
-    return brain.update()
+	return brain.update()
 
 
 func add_immunity(hazard: Type.Element) -> void:
-    _immunities[hazard] = true
+	_immunities[hazard] = true
 
 
 func has_immunity(hazard: Type.Element) -> bool:
-    return _immunities.has(hazard)
+	return _immunities.has(hazard)
 
 
 func remove_immunity(hazard: Type.Element) -> void:
-    _immunities.erase(hazard)
+	_immunities.erase(hazard)
 
 
 func reset_immunities() -> void:
-    _immunities = {}
+	_immunities = {}
 
 
 func _set_grid_position(pos: Vector2i) -> void:
-    super(pos)
-    if last_position == Vector2i():
-        last_direction = Direction.none
-    else:
-        last_direction = Direction.by_delta(last_position, grid_position)
+	super(pos)
+	if last_position == Vector2i():
+		last_direction = Direction.none
+	else:
+		last_direction = Direction.by_delta(last_position, grid_position)
