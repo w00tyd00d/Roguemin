c45a0e0074e70cc822614781f5a06042917d7996
w00t <w00tyd00d@gmail.com>
2026-08-02 13:11:58 -0400

    No more prefab folder

--- scripts/world/rooms/rooms.gd
+++ scripts/world/rooms/rooms.gd
@@ -1,18 +1,18 @@
 class_name Rooms extends Object
 
 
-static var TEST := preload("res://prefabs/rooms/test_room.tscn").instantiate()
+static var TEST := preload("uid://mwsjhul1qwks").instantiate()
 
-static var HOME_BASE := preload("res://prefabs/rooms/main_base_room.tscn").instantiate()
+static var HOME_BASE := preload("uid://n77rw20oy6ru").instantiate()
 
-static var ROOM_2X2: RoomBlueprint = preload("res://prefabs/rooms/room_2x2.tscn").instantiate()
-static var ROOM_2X3: RoomBlueprint = preload("res://prefabs/rooms/room_2x3.tscn").instantiate()
-static var ROOM_2X4: RoomBlueprint = preload("res://prefabs/rooms/room_2x4.tscn").instantiate()
-static var ROOM_3X2: RoomBlueprint = preload("res://prefabs/rooms/room_3x2.tscn").instantiate()
-static var ROOM_3X3: RoomBlueprint = preload("res://prefabs/rooms/room_3x3.tscn").instantiate()
-static var ROOM_3X4: RoomBlueprint = preload("res://prefabs/rooms/room_3x4.tscn").instantiate()
-static var ROOM_4X2: RoomBlueprint = preload("res://prefabs/rooms/room_4x2.tscn").instantiate()
-static var ROOM_4X3: RoomBlueprint = preload("res://prefabs/rooms/room_4x3.tscn").instantiate()
+static var ROOM_2X2: RoomBlueprint = preload("uid://bj4uvic1ud8po").instantiate()
+static var ROOM_2X3: RoomBlueprint = preload("uid://d33l7t766au5j").instantiate()
+static var ROOM_2X4: RoomBlueprint = preload("uid://bt5e55sf3e6cc").instantiate()
+static var ROOM_3X2: RoomBlueprint = preload("uid://dm7daebuk2shh").instantiate()
+static var ROOM_3X3: RoomBlueprint = preload("uid://dgkdc43u0h6s2").instantiate()
+static var ROOM_3X4: RoomBlueprint = preload("uid://bncy22p7brocd").instantiate()
+static var ROOM_4X2: RoomBlueprint = preload("uid://75gp6mlj3l34").instantiate()
+static var ROOM_4X3: RoomBlueprint = preload("uid://bbrle4xbg7b6c").instantiate()
 
 static var ALL_ROOMS : Array[RoomBlueprint] = [
     ROOM_2X2,
--- scripts/world/tilemeta.gd
+++
@@ -1,4 +0,0 @@
-class_name TileMeta extends Node
-
-## A container of various different metadata configs for each type of tile.
-
